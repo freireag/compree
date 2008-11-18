77caf35b438b9b304d5e645bd214c3c4f42b59d1
Thiago Freire <freireag@gmail.com>
2008-11-18 04:30:42 -0300

    Testes da action show para products_controller

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -10,4 +10,14 @@ class ProductsControllerTest < ActionController::TestCase
     
     should_not_set_the_flash
   end
+  
+  context "on GET to :show" do
+    setup { get :show, :id => 1 }
+    
+    should_render_template :show
+    should_respond_with :success
+    should_route :get, "/products/show/1", :action => :show, :id => 1
+
+    should_not_set_the_flash
+  end
 end
